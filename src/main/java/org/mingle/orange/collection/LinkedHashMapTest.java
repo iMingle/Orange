573a529a323774f6579053f0105c5673d5717da0
jinminglei <jinminglei@7e8e3328-a908-4e09-befc-a7153a5b8915>
2014-09-03 15:38:25 +0000

    最近最少使用方法测试
    git-svn-id: http://code.taobao.org/svn/mingle/trunk/Orange@55 7e8e3328-a908-4e09-befc-a7153a5b8915

--- src/main/java/org/mingle/orange/collection/LinkedHashMapTest.java
+++ src/main/java/org/mingle/orange/collection/LinkedHashMapTest.java
@@ -20,14 +20,28 @@ public class LinkedHashMapTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Map<String, String> linkedMap = new LinkedHashMap<String, String>();
+		Map<String, String> linked = new LinkedHashMap<String, String>(128, 0.75F, true);
+		linked.put("4", "four");
+		linked.put("5", "five");
+		linked.put("6", "six");
+		linked.put("1", "one");
+		linked.put("2", "two");
+		linked.put("3", "three");
+
+		Iterator<String> ita = linked.keySet().iterator();
+		while (ita.hasNext()) {
+			System.out.print(ita.next() + ",");
+		}
+		System.out.println();
+		
+		Map<String, String> linkedMap = new LinkedHashMap<String, String>(128, 0.75F, true);
 		linkedMap.put("4", "four");
 		linkedMap.put("5", "five");
 		linkedMap.put("6", "six");
 		linkedMap.put("1", "one");
 		linkedMap.put("2", "two");
 		linkedMap.put("3", "three");
-		
+		linkedMap.get("4");
 		Iterator<String> it = linkedMap.keySet().iterator();
 		while (it.hasNext()) {
 			System.out.print(it.next() + ",");
