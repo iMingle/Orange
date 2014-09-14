bcced88ab779cf6981ceb4a6a85de038dbb3b2c2
jinminglei <jinminglei@7e8e3328-a908-4e09-befc-a7153a5b8915>
2014-09-14 15:42:09 +0000

    1. 弹跳球多线程版upload
    git-svn-id: http://code.taobao.org/svn/mingle/trunk/Orange@69 7e8e3328-a908-4e09-befc-a7153a5b8915

--- src/main/java/org/mingle/orange/thread/BounceThread.java
+++ src/main/java/org/mingle/orange/thread/BounceThread.java
@@ -4,6 +4,7 @@
 package org.mingle.orange.thread;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
@@ -14,13 +15,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
- * shows an animated bouncing ball.
+ * shows animated bouncing ball.
  * 
  * @author <a href="mailto:jinminglei@yeah.net">mingle</a>
  * @date 2014年9月13日
  * @version 1.0
  */
-public class Bounce {
+public class BounceThread {
 
 	/**
 	 * @param args
@@ -40,6 +41,43 @@ public class Bounce {
 }
 
 /**
+ * a runnable that animates a bouncing ball.
+ */
+class BourceRunnable implements Runnable {
+	private Ball ball;
+	private Component component;
+	private static final int STEPS = 1000;
+	private static final int DELAY = 5;
+
+	/**
+	 * @param ball
+	 * @param component
+	 */
+	public BourceRunnable(Ball ball, Component component) {
+		super();
+		this.ball = ball;
+		this.component = component;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		try {
+			for (int i = 1; i < STEPS; i++) {
+				ball.move(component.getBounds());
+				component.repaint();
+				Thread.sleep(DELAY);
+			}
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+	}
+	
+}
+
+/**
  * the frame with ball component and buttons.
  */
 class BounceFrame extends JFrame {
@@ -51,8 +89,6 @@ class BounceFrame extends JFrame {
 	private BallComponent comp;
 	private static final int DEFAULT_WIDTH = 450;
 	private static final int DEFAULT_HEIGHT = 350;
-	private static final int STEPS = 1000;
-	private static final int DELAY = 3;
 
 	/**
 	 * Constructs the frame with the component for showing the bouncing ball and
@@ -100,20 +136,13 @@ class BounceFrame extends JFrame {
 	}
 	
 	/*
-	 * Adds a bouncing ball to the panel and makes it bounce 1,000 times.
+	 * Adds a bouncing ball to the panel and starts a thread to make it bounce.
 	 */
 	public void addBall() {
-		try {
-			Ball ball = new Ball();
-			comp.add(ball);
-			
-			for (int i = 1; i < STEPS; i++) {
-				ball.move(comp.getBounds());
-				comp.paint(comp.getGraphics());
-				Thread.sleep(DELAY);
-			}
-		} catch (InterruptedException e) {
-			// TODO: handle exception
-		}
+		Ball b = new Ball();
+		comp.add(b);
+		Runnable r = new BourceRunnable(b, comp);
+		Thread t = new Thread(r);
+		t.start();
 	}
 }
