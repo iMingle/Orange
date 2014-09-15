0f8538fadf4c618fa1751e16d6e40356204f0558
jinminglei@yeah.net <jinminglei@yeah.net@7e8e3328-a908-4e09-befc-a7153a5b8915>
2014-09-15 06:44:14 +0000

    1. add CollectionTest and MethodTest file
    git-svn-id: http://code.taobao.org/svn/mingle/trunk/Orange@70 7e8e3328-a908-4e09-befc-a7153a5b8915

---
+++ src/main/java/org/mingle/orange/test/CollectionTest.java
@@ -0,0 +1,45 @@
+/*
+ * (c) Copyright 2013 网神信息技术（北京）股份有限公司
+ * http://www.legendsec.com
+ */
+package org.mingle.orange.test;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ *
+ * @since 1.0 2014年7月22日
+ * @author <a href="mailto:jinml@legendsec.com">靳明雷</a>
+ * @version 1.0
+ */
+public class CollectionTest {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("1", "a");
+		map.put("2", "b");
+		map.put("3", "c");
+		map.put("4", "d");
+		map.put("5", "e");
+		map.put("6", "f");
+		map.put("7", "g");
+		map.put("8", "h");
+		map.put("9", "i");
+		map.put(null, null);
+		map.put(null, null);
+		
+		Iterator<Entry<String, String>> it = map.entrySet().iterator();
+		
+		System.out.println(map.get("10"));
+		System.out.println(map.get(null));
+		
+		if (it.hasNext()) {
+			// 执行一次next游标下移
+//			System.out.println("map's size = " + map.size());
+//			System.out.println(it.next().getKey());
+//			System.out.println(it.next().getKey());
+		}
+	}
+}
---
+++ src/main/java/org/mingle/orange/test/MethodTest.java
@@ -0,0 +1,35 @@
+/*
+ * (c) Copyright 2013 网神信息技术（北京）股份有限公司
+ * http://www.legendsec.com
+ */
+package org.mingle.orange.test;
+
+/**
+ *
+ * @since 1.0 2014年7月18日
+ * @author <a href="mailto:jinml@legendsec.com">靳明雷</a>
+ * @version 1.0
+ */
+public class MethodTest {
+	
+	/**
+	 * test if parameter will change.
+	 * @param id
+	 * @return
+	 */
+	public static Long returnLong(Long id) {
+		id += 1;
+		return id;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Long id = 12L;
+		
+		System.out.println(MethodTest.returnLong(id));
+		System.out.println("id = " + id);
+	}
+
+}
