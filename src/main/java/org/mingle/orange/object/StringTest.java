dd51bc4283a04336e9481874cbfeb4103d0fc036
jinminglei <jinminglei@7e8e3328-a908-4e09-befc-a7153a5b8915>
2014-09-20 02:57:42 +0000

    1. 转码测试
    git-svn-id: http://code.taobao.org/svn/mingle/trunk/Orange@79 7e8e3328-a908-4e09-befc-a7153a5b8915

---
+++ src/main/java/org/mingle/orange/object/StringTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2014, Mingle. All rights reserved.
+ */
+package org.mingle.orange.object;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * @author <a href="mailto:jinminglei@yeah.net">mingle</a>
+ * @version 1.0
+ */
+public class StringTest {
+	/**
+	 * 转码测试
+	 * @param source
+	 * @return
+	 */
+	public static String encode(String source, String beforeCharsetName, String afterCharsetName) {
+		String target = null;
+		try {
+			target = new String(source.getBytes(beforeCharsetName), afterCharsetName);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return target;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/* 转码测试 */
+		System.out.println("english" + StringTest.encode("abc", "GB2312", "ISO8859-1"));
+		System.out.println("汉语" + StringTest.encode("靳明雷", "GB2312", "GB2312"));
+		System.out.println("汉语" + StringTest.encode("靳明雷", "GB2312", "UTF-8"));
+		
+	}
+
+}
