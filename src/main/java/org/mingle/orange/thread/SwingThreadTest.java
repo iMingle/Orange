911eac1a26a8fd7a6659ecf6325e1b5ddc39cc17
jinminglei <jinminglei@7e8e3328-a908-4e09-befc-a7153a5b8915>
2014-09-20 09:16:25 +0000

    1. 线程和派发线程冲突测试
    git-svn-id: http://code.taobao.org/svn/mingle/trunk/Orange@81 7e8e3328-a908-4e09-befc-a7153a5b8915

---
+++ src/main/java/org/mingle/orange/thread/SwingThreadTest.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2014, Mingle. All rights reserved.
+ */
+package org.mingle.orange.thread;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * This program demonstrates that a thread that runs in parallel with the event
+ * dispatch thread can cause errors in Swing components.
+ * 
+ * @author <a href="mailto:jinminglei@yeah.net">mingle</a>
+ * @version 1.0
+ */
+public class SwingThreadTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				SwingThreadFrame frame = new SwingThreadFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+
+}
+
+/**
+ * This frame has two buttons to fill a combo box from a separate thread. The
+ * "Good" button uses the event queue, the "Bad" button modifies the combo box
+ * directly.
+ */
+class SwingThreadFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8917255486159489807L;
+
+	public SwingThreadFrame() {
+		setTitle("SwingThreadTest");
+
+		final JComboBox<Integer> combo = new JComboBox<Integer>();
+		combo.insertItemAt(Integer.MAX_VALUE, 0);
+		combo.setPrototypeDisplayValue(combo.getItemAt(0));
+		combo.setSelectedIndex(0);
+
+		JPanel panel = new JPanel();
+
+		JButton goodButton = new JButton("Good");
+		goodButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				new Thread(new GoodWorkerRunnable(combo)).start();
+			}
+		});
+		panel.add(goodButton);
+		JButton badButton = new JButton("Bad");
+		badButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				new Thread(new BadWorkerRunnable(combo)).start();
+			}
+		});
+		panel.add(badButton);
+
+		panel.add(combo);
+		add(panel);
+		pack();
+	}
+}
+
+/**
+ * This runnable modifies a combo box by randomly adding and removing numbers.
+ * This can result in errors because the combo box methods are not synchronized
+ * and both the worker thread and the event dispatch thread access the combo
+ * box.
+ */
+class BadWorkerRunnable implements Runnable {
+	private JComboBox<Integer> combo;
+	private Random generator;
+
+	public BadWorkerRunnable(JComboBox<Integer> aCombo) {
+		combo = aCombo;
+		generator = new Random();
+	}
+
+	public void run() {
+		try {
+			while (true) {
+				int i = Math.abs(generator.nextInt());
+				if (i % 2 == 0)
+					combo.insertItemAt(i, 0);
+				else if (combo.getItemCount() > 0)
+					combo.removeItemAt(i % combo.getItemCount());
+				Thread.sleep(1);
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+}
+
+/**
+ * This runnable modifies a combo box by randomly adding and removing numbers.
+ * In order to ensure that the combo box is not corrupted, the editing
+ * operations are forwarded to the event dispatch thread.
+ */
+class GoodWorkerRunnable implements Runnable {
+	private JComboBox<Integer> combo;
+	private Random generator;
+
+	public GoodWorkerRunnable(JComboBox<Integer> aCombo) {
+		combo = aCombo;
+		generator = new Random();
+	}
+
+	public void run() {
+		try {
+			while (true) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						int i = Math.abs(generator.nextInt());
+						if (i % 2 == 0)
+							combo.insertItemAt(i, 0);
+						else if (combo.getItemCount() > 0)
+							combo.removeItemAt(i % combo.getItemCount());
+					}
+				});
+				Thread.sleep(1);
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+}
