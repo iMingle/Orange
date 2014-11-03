3de7fe3e4f0872edb8592bbd177809649f43fb7f
jinminglei <jinminglei@yeah.net>
2014-11-03 20:22:51 +0800

    add files

---
+++ src/test/java/org/mingle/orange/test/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2014, Mingle. All rights reserved.
+ */
+/**
+ * @author <a href="mailto:jinminglei@yeah.net">Mingle</a>
+ * @date 2014年11月3日
+ * @version 1.0
+ */
+package org.mingle.orange.test;
