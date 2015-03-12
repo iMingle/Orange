bf11d54acc2bf36c6f5e1411f0f56e7a3945d945
jinminglei <jinminglei@yeah.net>
2015-03-13 01:28:54 +0800

    1. 二叉树遍历代码测试 2. hashCode和equals方法书写

---
+++ src/main/java/org/mingle/orange/object/HashCodeEquals.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2014, Mingle. All rights reserved.
+ */
+package org.mingle.orange.object;
+
+/**
+ * hashCode和equals方法如何写
+ * 
+ * @author <a href="mailto:jinminglei@yeah.net">mingle</a>
+ * @version 1.0
+ */
+public class HashCodeEquals {
+	private int age;
+	private String name;
+	
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HashCodeEquals other = (HashCodeEquals) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
