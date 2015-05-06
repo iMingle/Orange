2172e994b955a1bc1690017ff05cc90e8b42cc7b
jinminglei <jinminglei@yeah.net>
2015-05-07 00:14:55 +0800

    职责链,状态机,多路分发

--- src/main/java/org/mingle/orange/java/speciality/EnumTest.java
+++ src/main/java/org/mingle/orange/java/speciality/EnumTest.java
@@ -5,17 +5,21 @@ package org.mingle.orange.java.speciality;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.mingle.orange.java.speciality.Food.Appetizer;
+import org.mingle.orange.java.speciality.Food.Coffee;
 import org.mingle.orange.java.speciality.Food.Dessert;
 import org.mingle.orange.java.speciality.Food.MainCourse;
-import org.mingle.orange.java.speciality.Food.Coffee;
+import org.mingle.orange.java.util.TextFile;
 
 /**
  * 枚举测试
@@ -274,11 +278,9 @@ class TypeOfFood {
  * 枚举的枚举
  */
 enum Course {
-	APPETIZER(Food.Appetizer.class), 
-	MAINCOURSE(Food.MainCourse.class), 
-	DESSERT(Food.Dessert.class), 
-	COFFEE(Food.Coffee.class);
-	
+	APPETIZER(Food.Appetizer.class), MAINCOURSE(Food.MainCourse.class), DESSERT(
+			Food.Dessert.class), COFFEE(Food.Coffee.class);
+
 	private Food[] values;
 
 	private Course(Class<? extends Food> kind) {
@@ -288,7 +290,7 @@ enum Course {
 	public Food randomSelection() {
 		return Enums.random(values);
 	}
-	
+
 	public static void main(String[] args) {
 		// 生成菜单
 		for (int i = 0; i < 5; i++) {
@@ -331,16 +333,16 @@ enum SecurityCategory {
 			SecurityCategory category = Enums.random(SecurityCategory.class);
 			System.out.println(category + ": " + category.randomSelection());
 		}
-//		BOND: MUNICIPAL
-//		BOND: MUNICIPAL
-//		STOCK: MARGIN
-//		STOCK: MARGIN
-//		BOND: JUNK
-//		STOCK: SHORT
-//		STOCK: LONG
-//		STOCK: LONG
-//		BOND: MUNICIPAL
-//		BOND: JUNK
+		// BOND: MUNICIPAL
+		// BOND: MUNICIPAL
+		// STOCK: MARGIN
+		// STOCK: MARGIN
+		// BOND: JUNK
+		// STOCK: SHORT
+		// STOCK: LONG
+		// STOCK: LONG
+		// BOND: MUNICIPAL
+		// BOND: JUNK
 
 	}
 }
@@ -356,15 +358,18 @@ class EnumSets {
 	public static void main(String[] args) {
 		EnumSet<AlarmPoints> points = EnumSet.noneOf(AlarmPoints.class);
 		points.add(AlarmPoints.BATHROOM);
-		points.addAll(EnumSet.of(AlarmPoints.STAIR1, AlarmPoints.STAIR2, AlarmPoints.KITCHEN));
-		System.out.println(points);	// [STAIR1, STAIR2, BATHROOM, KITCHEN]
-		
+		points.addAll(EnumSet.of(AlarmPoints.STAIR1, AlarmPoints.STAIR2,
+				AlarmPoints.KITCHEN));
+		System.out.println(points); // [STAIR1, STAIR2, BATHROOM, KITCHEN]
+
 		points = EnumSet.allOf(AlarmPoints.class);
-		points.removeAll(EnumSet.of(AlarmPoints.STAIR1, AlarmPoints.STAIR2, AlarmPoints.KITCHEN));
-		System.out.println(points);	// [LOBBY, OFFICE1, OFFICE2, OFFICE3, OFFICE4, BATHROOM, UTILITY]
-		
+		points.removeAll(EnumSet.of(AlarmPoints.STAIR1, AlarmPoints.STAIR2,
+				AlarmPoints.KITCHEN));
+		System.out.println(points); // [LOBBY, OFFICE1, OFFICE2, OFFICE3,
+									// OFFICE4, BATHROOM, UTILITY]
+
 		points = EnumSet.complementOf(points);
-		System.out.println(points);	// [STAIR1, STAIR2, KITCHEN]
+		System.out.println(points); // [STAIR1, STAIR2, KITCHEN]
 	}
 }
 
@@ -376,15 +381,15 @@ class EnumMaps {
 	public static void main(String[] args) {
 		EnumMap<AlarmPoints, Command> em = new EnumMap<>(AlarmPoints.class);
 		em.put(AlarmPoints.KITCHEN, new Command() {
-			
+
 			@Override
 			public void action() {
 				System.out.println("kitchen fire!");
 			}
 		});
-		
+
 		em.put(AlarmPoints.BATHROOM, new Command() {
-			
+
 			@Override
 			public void action() {
 				System.out.println("bathroom alert!");
@@ -404,3 +409,734 @@ class EnumMaps {
 	}
 }
 
+/**
+ * 常量相关的方法
+ */
+enum ConstantSpecificMethod {
+	DATE_TIME {
+		String getInfo() {
+			return DateFormat.getDateInstance().format(new Date());
+		}
+	},
+	CLASSPATH {
+		String getInfo() {
+			return System.getenv("CLASSPATH");
+		}
+	},
+	VERSION {
+		String getInfo() {
+			return System.getProperty("java.version");
+		}
+	};
+	abstract String getInfo();
+
+	public static void main(String[] args) {
+		for (ConstantSpecificMethod csm : values()) {
+			System.out.println(csm.getInfo());
+		}
+	}
+}
+
+/**
+ * enum职责链,投递邮件
+ */
+class Mail {
+	// The NO's lower the probability of random selection:
+	enum GeneralDelivery {
+		YES, NO1, NO2, NO3, NO4, NO5
+	}
+
+	enum Scannability {
+		UNSCANNABLE, YES1, YES2, YES3, YES4
+	}
+
+	enum Readability {
+		ILLEGIBLE, YES1, YES2, YES3, YES4
+	}
+
+	enum Address {
+		INCORRECT, OK1, OK2, OK3, OK4, OK5, OK6
+	}
+
+	enum ReturnAddress {
+		MISSING, OK1, OK2, OK3, OK4, OK5
+	}
+
+	GeneralDelivery generalDelivery;
+	Scannability scannability;
+	Readability readability;
+	Address address;
+	ReturnAddress returnAddress;
+	static long counter = 0;
+	long id = counter++;
+
+	public String toString() {
+		return "Mail " + id;
+	}
+
+	public String details() {
+		return toString() + ", General Delivery: " + generalDelivery
+				+ ", Address Scanability: " + scannability
+				+ ", Address Readability: " + readability
+				+ ", Address Address: " + address + ", Return address: "
+				+ returnAddress;
+	}
+
+	public static Mail randomMail() {
+		Mail m = new Mail();
+		m.generalDelivery = Enums.random(GeneralDelivery.class);
+		m.scannability = Enums.random(Scannability.class);
+		m.readability = Enums.random(Readability.class);
+		m.address = Enums.random(Address.class);
+		m.returnAddress = Enums.random(ReturnAddress.class);
+		return m;
+	}
+
+	public static Iterable<Mail> generator(final int count) {
+		return new Iterable<Mail>() {
+			int n = count;
+
+			@Override
+			public Iterator<Mail> iterator() {
+				return new Iterator<Mail>() {
+
+					@Override
+					public Mail next() {
+						return randomMail();
+					}
+
+					@Override
+					public boolean hasNext() {
+						return n-- > 0;
+					}
+				};
+			}
+		};
+	}
+}
+
+class PostOffice {
+	enum MailHandler {
+		GENERAL_DELIVERY {
+			boolean handle(Mail m) {
+				switch (m.generalDelivery) {
+				case YES:
+					System.out.println("Using general delivery for " + m);
+					return true;
+				default:
+					return false;
+				}
+			}
+		},
+		MACHINE_SCAN {
+			boolean handle(Mail m) {
+				switch (m.scannability) {
+				case UNSCANNABLE:
+					return false;
+				default:
+					switch (m.address) {
+					case INCORRECT:
+						return false;
+					default:
+						System.out
+								.println("Delivering " + m + " automatically");
+						return true;
+					}
+				}
+			}
+		},
+		VISUAL_INSPECTION {
+			boolean handle(Mail m) {
+				switch (m.readability) {
+				case ILLEGIBLE:
+					return false;
+				default:
+					switch (m.address) {
+					case INCORRECT:
+						return false;
+					default:
+						System.out.println("Delivering " + m + " normally");
+						return true;
+					}
+				}
+			}
+		},
+		RETURN_TO_SENDER {
+			boolean handle(Mail m) {
+				switch (m.returnAddress) {
+				case MISSING:
+					return false;
+				default:
+					System.out.println("Returning " + m + " to sender");
+					return true;
+				}
+			}
+		};
+		abstract boolean handle(Mail m);
+	}
+
+	static void handle(Mail m) {
+		for (MailHandler handler : MailHandler.values())
+			if (handler.handle(m))
+				return;
+		System.out.println(m + " is a dead letter");
+	}
+
+	public static void main(String[] args) {
+		for (Mail mail : Mail.generator(10)) {
+			System.out.println(mail.details());
+			handle(mail);
+			System.out.println("*****");
+		}
+	}
+}
+
+/**
+ * enum状态机,自动售货机
+ */
+enum Input {
+	NICKEL(5), DIME(10), QUARTER(25), DOLLAR(100), TOOTHPASTE(200), CHIPS(75), SODA(
+			100), SOAP(50), ABORT_TRANSACTION {
+		public int amount() { // Disallow
+			throw new RuntimeException("ABORT.amount()");
+		}
+	},
+	STOP { // This must be the last instance.
+		public int amount() { // Disallow
+			throw new RuntimeException("SHUT_DOWN.amount()");
+		}
+	};
+	int value; // In cents
+
+	Input(int value) {
+		this.value = value;
+	}
+
+	Input() {
+	}
+
+	int amount() {
+		return value;
+	}; // In cents
+
+	static Random rand = new Random(47);
+
+	public static Input randomSelection() {
+		// Don't include STOP:
+		return values()[rand.nextInt(values().length - 1)];
+	}
+}
+
+/**
+ * 货物类别
+ */
+enum Category {
+	MONEY(Input.NICKEL, Input.DIME, Input.QUARTER, Input.DOLLAR), ITEM_SELECTION(
+			Input.TOOTHPASTE, Input.CHIPS, Input.SODA, Input.SOAP), QUIT_TRANSACTION(
+			Input.ABORT_TRANSACTION), SHUT_DOWN(Input.STOP);
+	private Input[] values;
+
+	Category(Input... types) {
+		values = types;
+	}
+
+	private static EnumMap<Input, Category> categories = new EnumMap<Input, Category>(
+			Input.class);
+	static {
+		for (Category c : Category.class.getEnumConstants())
+			for (Input type : c.values)
+				categories.put(type, c);
+	}
+
+	public static Category categorize(Input input) {
+		return categories.get(input);
+	}
+}
+
+/**
+ * 售货机
+ */
+class VendingMachine {
+	private static State state = State.RESTING;
+	private static int amount = 0;
+	private static Input selection = null;
+
+	enum StateDuration {
+		TRANSIENT
+	} // Tagging enum
+
+	enum State {
+		RESTING {
+			void next(Input input) {
+				switch (Category.categorize(input)) {
+				case MONEY:
+					amount += input.amount();
+					state = ADDING_MONEY;
+					break;
+				case SHUT_DOWN:
+					state = TERMINAL;
+				default:
+				}
+			}
+		},
+		ADDING_MONEY {
+			void next(Input input) {
+				switch (Category.categorize(input)) {
+				case MONEY:
+					amount += input.amount();
+					break;
+				case ITEM_SELECTION:
+					selection = input;
+					if (amount < selection.amount())
+						System.out.println("Insufficient money for "
+								+ selection);
+					else
+						state = DISPENSING;
+					break;
+				case QUIT_TRANSACTION:
+					state = GIVING_CHANGE;
+					break;
+				case SHUT_DOWN:
+					state = TERMINAL;
+				default:
+				}
+			}
+		},
+		DISPENSING(StateDuration.TRANSIENT) {
+			void next() {
+				System.out.println("here is your " + selection);
+				amount -= selection.amount();
+				state = GIVING_CHANGE;
+			}
+		},
+		GIVING_CHANGE(StateDuration.TRANSIENT) {
+			void next() {
+				if (amount > 0) {
+					System.out.println("Your change: " + amount);
+					amount = 0;
+				}
+				state = RESTING;
+			}
+		},
+		TERMINAL {
+			void output() {
+				System.out.println("Halted");
+			}
+		};
+		private boolean isTransient = false;
+
+		State() {
+		}
+
+		State(StateDuration trans) {
+			isTransient = true;
+		}
+
+		void next(Input input) {
+			throw new RuntimeException("Only call "
+					+ "next(Input input) for non-transient states");
+		}
+
+		void next() {
+			throw new RuntimeException("Only call next() for "
+					+ "StateDuration.TRANSIENT states");
+		}
+
+		void output() {
+			System.out.println(amount);
+		}
+	}
+
+	static void run(Generator<Input> gen) {
+		while (state != State.TERMINAL) {
+			state.next(gen.next());
+			while (state.isTransient)
+				state.next();
+			state.output();
+		}
+	}
+
+	public static void main(String[] args) {
+		Generator<Input> gen = new RandomInputGenerator();
+		if (args.length == 1)
+			gen = new FileInputGenerator(args[0]);
+		run(gen);
+	}
+}
+
+class RandomInputGenerator implements Generator<Input> {
+	public Input next() {
+		return Input.randomSelection();
+	}
+}
+
+// Create Inputs from a file of ';'-separated strings:
+class FileInputGenerator implements Generator<Input> {
+	private Iterator<String> input;
+
+	public FileInputGenerator(String fileName) {
+		input = new TextFile(fileName, ";").iterator();
+	}
+
+	public Input next() {
+		if (!input.hasNext())
+			return null;
+		return Enum.valueOf(Input.class, input.next().trim());
+	}
+}
+
+/**
+ * 多路分发,输赢结果
+ */
+enum Outcome {
+	WIN, LOSE, DRAW
+}
+
+interface Item {
+	Outcome compete(Item it);
+	Outcome eval(Paper p);	// 布
+	Outcome eval(Scissors s);	// 剪刀
+	Outcome eval(Rock r);	// 石头
+}
+
+class Paper implements Item {
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#compete(org.mingle.orange.java.speciality.Item)
+	 */
+	@Override
+	public Outcome compete(Item it) {
+		return it.eval(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Paper)
+	 */
+	@Override
+	public Outcome eval(Paper p) {
+		return Outcome.DRAW;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Scissors)
+	 */
+	@Override
+	public Outcome eval(Scissors s) {
+		return Outcome.WIN;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Rock)
+	 */
+	@Override
+	public Outcome eval(Rock r) {
+		return Outcome.LOSE;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Paper";
+	}
+	
+}
+
+class Scissors implements Item {
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#compete(org.mingle.orange.java.speciality.Item)
+	 */
+	@Override
+	public Outcome compete(Item it) {
+		return it.eval(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Paper)
+	 */
+	@Override
+	public Outcome eval(Paper p) {
+		return Outcome.LOSE;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Scissors)
+	 */
+	@Override
+	public Outcome eval(Scissors s) {
+		return Outcome.DRAW;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Rock)
+	 */
+	@Override
+	public Outcome eval(Rock r) {
+		return Outcome.WIN;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Scissors";
+	}
+	
+}
+
+class Rock implements Item {
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#compete(org.mingle.orange.java.speciality.Item)
+	 */
+	@Override
+	public Outcome compete(Item it) {
+		return it.eval(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Paper)
+	 */
+	@Override
+	public Outcome eval(Paper p) {
+		return Outcome.WIN;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Scissors)
+	 */
+	@Override
+	public Outcome eval(Scissors s) {
+		return Outcome.LOSE;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Item#eval(org.mingle.orange.java.speciality.Rock)
+	 */
+	@Override
+	public Outcome eval(Rock r) {
+		return Outcome.DRAW;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Rock";
+	}
+	
+}
+
+class RoShamBo {
+	public static <T extends Competitor<T>> void match(T a, T b) {
+		System.out.println(a + " vs. " + b + ": " + a.compete(b));
+	}
+
+	public static <T extends Enum<T> & Competitor<T>> void play(
+			Class<T> rsbClass, int size) {
+		for (int i = 0; i < size; i++)
+			match(Enums.random(rsbClass), Enums.random(rsbClass));
+	}
+}
+
+/**
+ * 石头剪刀布
+ */
+class RoShamBo1 {
+	static final int SIZE = 20;
+	private static Random rand = new Random(47);
+	
+	public static Item newItem() {
+		switch (rand.nextInt(3)) {
+		case 0:
+			return new Scissors();
+		case 1:
+			return new Paper();
+		case 2:
+			return new Rock();
+		default:
+			return null;
+		}
+	}
+	
+	public static void match(Item a, Item b) {
+		System.out.println(a + " vs. " + b + ": " + a.compete(b));
+	}
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < SIZE; i++) {
+			match(newItem(), newItem());
+		}
+	}
+}
+
+/**
+ * 使用enum分发
+ */
+interface Competitor<T extends Competitor<T>> {
+	Outcome compete(T competitor);
+}
+
+enum RoShamBo2 implements Competitor<RoShamBo2> {
+	PAPER(Outcome.DRAW, Outcome.LOSE, Outcome.WIN), 
+	SCISSORS(Outcome.WIN, Outcome.DRAW, Outcome.LOSE), 
+	ROCK(Outcome.LOSE, Outcome.WIN, Outcome.DRAW);
+	
+	private Outcome vPAPER, vSCISSORS, vROCK;
+
+	/**
+	 * @param vPAPER
+	 * @param vSCISSORS
+	 * @param vROCK
+	 */
+	private RoShamBo2(Outcome vPAPER, Outcome vSCISSORS, Outcome vROCK) {
+		this.vPAPER = vPAPER;
+		this.vSCISSORS = vSCISSORS;
+		this.vROCK = vROCK;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mingle.orange.java.speciality.Competitor#compete(org.mingle.orange.java.speciality.Competitor)
+	 */
+	@Override
+	public Outcome compete(RoShamBo2 it) {
+		switch (it) {
+		case PAPER:
+			return vPAPER;
+		case SCISSORS:
+			return vSCISSORS;
+		case ROCK:
+			return vROCK;
+		default:
+			return null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		RoShamBo.play(RoShamBo2.class, 20);
+	}
+}
+
+enum RoShamBo3 implements Competitor<RoShamBo3> {
+	PAPER {
+		public Outcome compete(RoShamBo3 it) {
+			switch (it) {
+			default: // To placate the compiler
+			case PAPER:
+				return Outcome.DRAW;
+			case SCISSORS:
+				return Outcome.LOSE;
+			case ROCK:
+				return Outcome.WIN;
+			}
+		}
+	},
+	SCISSORS {
+		public Outcome compete(RoShamBo3 it) {
+			switch (it) {
+			default:
+			case PAPER:
+				return Outcome.WIN;
+			case SCISSORS:
+				return Outcome.DRAW;
+			case ROCK:
+				return Outcome.LOSE;
+			}
+		}
+	},
+	ROCK {
+		public Outcome compete(RoShamBo3 it) {
+			switch (it) {
+			default:
+			case PAPER:
+				return Outcome.LOSE;
+			case SCISSORS:
+				return Outcome.WIN;
+			case ROCK:
+				return Outcome.DRAW;
+			}
+		}
+	};
+	public abstract Outcome compete(RoShamBo3 it);
+
+	public static void main(String[] args) {
+		RoShamBo.play(RoShamBo3.class, 20);
+	}
+}
+
+enum RoShamBo4 implements Competitor<RoShamBo4> {
+	ROCK {
+		public Outcome compete(RoShamBo4 opponent) {
+			return compete(SCISSORS, opponent);
+		}
+	},
+	SCISSORS {
+		public Outcome compete(RoShamBo4 opponent) {
+			return compete(PAPER, opponent);
+		}
+	},
+	PAPER {
+		public Outcome compete(RoShamBo4 opponent) {
+			return compete(ROCK, opponent);
+		}
+	};
+	
+	Outcome compete(RoShamBo4 loser, RoShamBo4 opponent) {
+		return ((opponent == this) ? Outcome.DRAW
+				: ((opponent == loser) ? Outcome.WIN : Outcome.LOSE));
+	}
+
+	public static void main(String[] args) {
+		RoShamBo.play(RoShamBo4.class, 20);
+	}
+}
+
+enum RoShamBo5 implements Competitor<RoShamBo5> {
+	PAPER, SCISSORS, ROCK;
+	static EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>> table = new EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>>(
+			RoShamBo5.class);
+	static {
+		for (RoShamBo5 it : RoShamBo5.values())
+			table.put(it, new EnumMap<RoShamBo5, Outcome>(RoShamBo5.class));
+		initRow(PAPER, Outcome.DRAW, Outcome.LOSE, Outcome.WIN);
+		initRow(SCISSORS, Outcome.WIN, Outcome.DRAW, Outcome.LOSE);
+		initRow(ROCK, Outcome.LOSE, Outcome.WIN, Outcome.DRAW);
+	}
+
+	static void initRow(RoShamBo5 it, Outcome vPAPER, Outcome vSCISSORS,
+			Outcome vROCK) {
+		EnumMap<RoShamBo5, Outcome> row = RoShamBo5.table.get(it);
+		row.put(RoShamBo5.PAPER, vPAPER);
+		row.put(RoShamBo5.SCISSORS, vSCISSORS);
+		row.put(RoShamBo5.ROCK, vROCK);
+	}
+
+	public Outcome compete(RoShamBo5 it) {
+		return table.get(this).get(it);
+	}
+
+	public static void main(String[] args) {
+		RoShamBo.play(RoShamBo5.class, 20);
+	}
+}
+
+enum RoShamBo6 implements Competitor<RoShamBo6> {
+	PAPER, SCISSORS, ROCK;
+	private static Outcome[][] table = {
+			{ Outcome.DRAW, Outcome.LOSE, Outcome.WIN }, // PAPER
+			{ Outcome.WIN, Outcome.DRAW, Outcome.LOSE }, // SCISSORS
+			{ Outcome.LOSE, Outcome.WIN, Outcome.DRAW }, // ROCK
+	};
+
+	public Outcome compete(RoShamBo6 other) {
+		return table[this.ordinal()][other.ordinal()];
+	}
+
+	public static void main(String[] args) {
+		RoShamBo.play(RoShamBo6.class, 20);
+	}
+}
