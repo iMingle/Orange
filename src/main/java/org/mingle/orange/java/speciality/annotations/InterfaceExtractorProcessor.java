89cd81738a2bfc8d1eaee42557e171c3062b5c86
jinminglei <jinminglei@yeah.net>
2015-05-12 00:21:56 +0800

    注解相关测试

--- src/main/java/org/mingle/orange/java/speciality/annotations/InterfaceExtractorProcessor.java
+++ src/main/java/org/mingle/orange/java/speciality/annotations/InterfaceExtractorProcessor.java
@@ -8,18 +8,20 @@ import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-
-import com.sun.jnlp.ApiDialog.DialogResult;
 
 /**
  *
  * @author <a href="mailto:jinminglei@yeah.net">mingle</a>
  * @version 1.0
  */
+@SupportedAnnotationTypes({"org.mingle.orange.java.speciality.annotations.ExtractInterface"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class InterfaceExtractorProcessor extends AbstractProcessor {
 
 	/* (non-Javadoc)
