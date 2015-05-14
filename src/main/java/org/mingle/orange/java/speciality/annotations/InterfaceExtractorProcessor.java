06e8eb78a1067784dfee45b4d93cb1e65bd2c6ff
jinminglei <jinminglei@yeah.net>
2015-05-15 00:04:18 +0800

    注解生成接口文件上传

--- src/main/java/org/mingle/orange/java/speciality/annotations/InterfaceExtractorProcessor.java
+++ src/main/java/org/mingle/orange/java/speciality/annotations/InterfaceExtractorProcessor.java
@@ -8,15 +8,16 @@ import java.io.PrintWriter;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
+import javax.lang.model.element.VariableElement;
 import javax.tools.JavaFileObject;
 
 /**
@@ -34,41 +35,46 @@ public class InterfaceExtractorProcessor extends AbstractProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment roundEnv) {
-		Messager messager = processingEnv.getMessager();
 		PrintWriter writer = null;
 		JavaFileObject sourceFile = null;
 		for (TypeElement typeElement : annotations) {
-			for (Element e : roundEnv.getElementsAnnotatedWith(typeElement)) {
-				if (ElementKind.CLASS == e.getKind()) {
-					for (Element e1 : e.getEnclosedElements()) {
-						if (ElementKind.METHOD == e1.getKind()) {
-							try {
-								sourceFile = processingEnv.getFiler().createSourceFile("IMultiply", e1);
-							} catch (IOException e2) {
-								e2.printStackTrace();
+			for (Element element : roundEnv.getElementsAnnotatedWith(typeElement)) {
+				if (ElementKind.CLASS == element.getKind()) {
+					try {
+						sourceFile = processingEnv.getFiler().createSourceFile(element.getAnnotation(ExtractInterface.class).value());
+						writer = new PrintWriter(sourceFile.openWriter());
+						writer.println(element.getEnclosingElement() + ";");
+						writer.println("public interface " + element.getAnnotation(ExtractInterface.class).value() + " {");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					for (Element e : element.getEnclosedElements()) {
+						if (ElementKind.METHOD == e.getKind()) {
+							if (e.getModifiers().contains(Modifier.PUBLIC) && !e.getModifiers().contains(Modifier.STATIC)) {
+								ExecutableElement ee = (ExecutableElement) e;
+								writer.print("	public ");
+								writer.print(ee.getReturnType() + " ");
+								writer.print(ee.getSimpleName() + "(");
+								int i = 0;
+								for (VariableElement parm : ee.getParameters()) {
+									String body = parm.getEnclosingElement().toString();
+									body = body.substring(body.indexOf('(') + 1, body.indexOf(')'));
+									String[] parms = body.split(",");
+									if (i == parms.length - 1) {
+										writer.print(parms[i++] + " " + parm.getSimpleName());
+									} else {
+										writer.print(parms[i++] + " " + parm.getSimpleName() + ", ");
+									}
+								}
+								writer.println(");");
 							}
 						}
 					}
+					
+					writer.println('}');
+					writer.close();
 				}
-				
-				
-				
-				System.out.println(e.getEnclosedElements());
-				System.out.println(e.getAnnotation(ExtractInterface.class));
-				System.out.println(e.getAnnotationMirrors().get(0));
-				System.out.println(e.getClass());
-				System.out.println(e.getEnclosingElement());
-				System.out.println(e.getKind());
-				System.out.println(e.getModifiers());
-				System.out.println(e.getSimpleName());
-//				Multiply(),multiply(int,int),add(int,int),main(java.lang.String[])
-//				@org.mingle.orange.java.speciality.annotations.ExtractInterface(value=IMultiply)
-//				@org.mingle.orange.java.speciality.annotations.ExtractInterface("IMultiply")
-//				class com.sun.tools.javac.code.Symbol$ClassSymbol
-//				org.mingle.orange.java.speciality.annotations
-//				CLASS
-//				[public]
-//				Multiply
 			}
 		}
 		return true;
