84ff729f5c1769f833e97fe4e99d993c80fad315
jinminglei <jinminglei@yeah.net>
2015-06-08 13:20:06 +0800

    计数信号量测试代码

---
+++ src/main/java/org/mingle/orange/java/speciality/concurrency/Fat.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.java.speciality.concurrency;
+
+/**
+ * 构造代码高昂的对象
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class Fat {
+	@SuppressWarnings("unused")
+	private volatile double d;	// 阻止优化
+	private static int counter = 0;
+	private final int id = counter++;
+	
+	public Fat() {
+		for (int i = 0; i < 10000; i++) {
+			d += (Math.PI + Math.E) / (double)i;
+		}
+	}
+	
+	public void operation() {
+		System.out.println(this);
+	}
+	
+	public String toString() {
+		return "Fat id: " + id;
+	}
+}
---
+++ src/main/java/org/mingle/orange/java/speciality/concurrency/Pool.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.java.speciality.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Semaphore测试,计数信号量允许n个任务同时访问一个资源
+ * 对象池,管理数量有限的对象
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class Pool<T> {
+	private int size;
+	private List<T> items = new ArrayList<T>();
+	private volatile boolean[] checkedOut;
+	private Semaphore avaliable;
+	
+	public Pool(Class<T> classObject, int size) {
+		this.size = size;
+		checkedOut = new boolean[size];
+		avaliable = new Semaphore(size, true);	// first-in first-out
+		for (int i = 0; i < size; i++) {
+			try {
+				items.add(classObject.newInstance());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	
+	public T checkOut() throws InterruptedException {
+		avaliable.acquire();
+		return getItem();
+	}
+	
+	public void checkIn(T x) {
+		if (releaseItem(x))
+			avaliable.release();
+	}
+	
+	private synchronized T getItem() {
+		for (int i = 0; i < size; i++) {
+			if (!checkedOut[i]) {
+				checkedOut[i] = true;
+				return items.get(i);
+			}
+		}
+		return null;
+	}
+	
+	private synchronized boolean releaseItem(T item) {
+		int index = items.indexOf(item);
+		if (index == -1) return false;
+		if (checkedOut[index]) {
+			checkedOut[index] = false;
+			return true;
+		}
+		return false;
+	}
+	
+}
