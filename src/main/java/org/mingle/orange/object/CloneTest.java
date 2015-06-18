3ed673e0e2f045690f41eaedab921c17c38f9c55
jinminglei <jinminglei@yeah.net>
2015-06-19 00:01:43 +0800

    代码格式修改

--- src/main/java/org/mingle/orange/object/CloneTest.java
+++ src/main/java/org/mingle/orange/object/CloneTest.java
@@ -11,80 +11,74 @@ import java.util.*;
  * @since 1.8
  * @author Mingle
  */
-public class CloneTest
-{
-   public static void main(String[] args)
-   {
-      try
-      {
-         Employee original = new Employee("John Q. Public", 50000);
-         original.setHireDay(2000, 1, 1);
-         Employee copy = original.clone();
-         copy.raiseSalary(10);
-         copy.setHireDay(2002, 12, 31);
-         System.out.println("original=" + original);
-         System.out.println("copy=" + copy);
-         
-         System.out.println("original: " + original.getHireDay().hashCode());
-         System.out.println("copy: " + copy.getHireDay().hashCode());
-      }
-      catch (CloneNotSupportedException e)
-      {
-         e.printStackTrace();
-      }
-   }
+public class CloneTest {
+	public static void main(String[] args) {
+		try {
+			Employee original = new Employee("John Q. Public", 50000);
+			original.setHireDay(2000, 1, 1);
+			Employee copy = original.clone();
+			copy.raiseSalary(10);
+			copy.setHireDay(2002, 12, 31);
+			System.out.println("original=" + original);
+			System.out.println("copy=" + copy);
+
+			System.out.println("original: " + original.getHireDay().hashCode());
+			System.out.println("copy: " + copy.getHireDay().hashCode());
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
-class Employee implements Cloneable
-{
-   public Employee(String n, double s)
-   {
-      name = n;
-      salary = s;
-      hireDay = new Date();
-   }
+class Employee implements Cloneable {
+	public Employee(String n, double s) {
+		name = n;
+		salary = s;
+		hireDay = new Date();
+	}
 
-   public Employee clone() throws CloneNotSupportedException
-   {
-      // call Object.clone()
-      Employee cloned = (Employee) super.clone();
+	public Employee clone() throws CloneNotSupportedException {
+		// call Object.clone()
+		Employee cloned = (Employee) super.clone();
 
-      // clone mutable fields
-      cloned.hireDay = (Date) hireDay.clone();
+		// clone mutable fields
+		cloned.hireDay = (Date) hireDay.clone();
 
-      return cloned;
-   }
+		return cloned;
+	}
 
-   /**
-    * Set the hire day to a given date. 
-    * @param year the year of the hire day
-    * @param month the month of the hire day
-    * @param day the day of the hire day
-    */
-   public void setHireDay(int year, int month, int day)
-   {
-      Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
-      
-      // Example of instance field mutation
-      hireDay.setTime(newHireDay.getTime());
-   }
-   
-   public Date getHireDay() {
-	   return hireDay;
-   }
+	/**
+	 * Set the hire day to a given date.
+	 * 
+	 * @param year
+	 *            the year of the hire day
+	 * @param month
+	 *            the month of the hire day
+	 * @param day
+	 *            the day of the hire day
+	 */
+	public void setHireDay(int year, int month, int day) {
+		Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
 
-   public void raiseSalary(double byPercent)
-   {
-      double raise = salary * byPercent / 100;
-      salary += raise;
-   }
+		// Example of instance field mutation
+		hireDay.setTime(newHireDay.getTime());
+	}
 
-   public String toString()
-   {
-      return "Employee[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay + "]";
-   }
+	public Date getHireDay() {
+		return hireDay;
+	}
 
-   private String name;
-   private double salary;
-   private Date hireDay;
+	public void raiseSalary(double byPercent) {
+		double raise = salary * byPercent / 100;
+		salary += raise;
+	}
+
+	public String toString() {
+		return "Employee[name=" + name + ",salary=" + salary + ",hireDay="
+				+ hireDay + "]";
+	}
+
+	private String name;
+	private double salary;
+	private Date hireDay;
 }
