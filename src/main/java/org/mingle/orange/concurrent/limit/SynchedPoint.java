e0332205380b020e9fa0940efc02e6543813569f
jinminglei <jinminglei@yeah.net>
2015-09-17 01:05:27 +0800

    1. 同步方法 2. 限制的方式 3. 构造类和重构类

---
+++ src/main/java/org/mingle/orange/concurrent/limit/SynchedPoint.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.limit;
+
+/**
+ * 代理非同步对象
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class SynchedPoint {
+	protected final BarePoint delegate = new BarePoint();
+
+	public synchronized double getX() {
+		return delegate.x;
+	}
+
+	public synchronized double getY() {
+		return delegate.y;
+	}
+
+	public synchronized void setX(double v) {
+		delegate.x = v;
+	}
+
+	public synchronized void setY(double v) {
+		delegate.y = v;
+	}
+}
+
+class BarePoint {
+	public double x;
+	public double y;
+}
---
+++ src/main/java/org/mingle/orange/concurrent/sync/Cell.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.sync;
+
+/**
+ * 死锁
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class Cell {
+	private long value;
+	
+	public synchronized long getValue() {
+		return value;
+	}
+
+	public synchronized void setValue(long v) {
+		value = v;
+	}
+
+	/**
+	 * 会造成死锁,a.swapValue(b)和b.swapValue(a)
+	 * 
+	 * @param other
+	 */
+	public synchronized void swapValue(Cell other) {
+		long t = getValue();
+		long v = other.getValue();
+		setValue(v);
+		other.setValue(t);
+	}
+}
