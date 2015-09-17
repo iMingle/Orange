aa21793408485d67cfc17a33526838eaa7e031c7
jinminglei <jinminglei@yeah.net>
2015-09-17 10:16:26 +0800

    同步时的遍历方式

---
+++ src/main/java/org/mingle/orange/concurrent/sync/Procedure.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.sync;
+
+/**
+ * 遍历-同步聚合操作
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public interface Procedure {
+	void apply(Object obj);
+}
