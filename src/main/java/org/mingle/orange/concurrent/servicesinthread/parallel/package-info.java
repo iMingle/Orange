01e221d9f8638879a084cd4febfd2ba982f45efa
jinminglei <jinminglei@yeah.net>
2015-09-24 18:36:43 +0800

    并行分解

---
+++ src/main/java/org/mingle/orange/concurrent/servicesinthread/parallel/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+/**
+ * 并行分解
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+package org.mingle.orange.concurrent.servicesinthread.parallel;
