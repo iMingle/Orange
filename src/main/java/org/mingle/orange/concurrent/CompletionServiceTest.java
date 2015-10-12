2b9eabeb3209c6453d57c1cc3478a61963828635
jinminglei <jinminglei@yeah.net>
2015-10-12 23:25:46 +0800

    Future和CompletionService

---
+++ src/main/java/org/mingle/orange/concurrent/CompletionServiceTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import org.mingle.orange.cache.self.LaunderThrowable;
+
+/**
+ * 
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public abstract class CompletionServiceTest {
+	private final ExecutorService executor;
+
+	CompletionServiceTest(ExecutorService executor) {
+		this.executor = executor;
+	}
+
+	void renderPage(CharSequence source) {
+		final List<ImageInfo> info = scanForImageInfo(source);
+		CompletionService<ImageData> completionService = new ExecutorCompletionService<ImageData>(
+				executor);
+		for (final ImageInfo imageInfo : info)
+			completionService.submit(new Callable<ImageData>() {
+				public ImageData call() {
+					return imageInfo.downloadImage();
+				}
+			});
+
+		renderText(source);
+
+		try {
+			for (int t = 0, n = info.size(); t < n; t++) {
+				Future<ImageData> f = completionService.take();
+				ImageData imageData = f.get();
+				renderImage(imageData);
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		} catch (ExecutionException e) {
+			throw LaunderThrowable.launderThrowable(e.getCause());
+		}
+	}
+
+	interface ImageData {
+	}
+
+	interface ImageInfo {
+		ImageData downloadImage();
+	}
+
+	abstract void renderText(CharSequence s);
+
+	abstract List<ImageInfo> scanForImageInfo(CharSequence s);
+
+	abstract void renderImage(ImageData i);
+}
---
+++ src/main/java/org/mingle/orange/concurrent/FutureRenderer.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.mingle.orange.cache.self.LaunderThrowable;
+
+/**
+ * 
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public abstract class FutureRenderer {
+	private final ExecutorService executor = Executors.newCachedThreadPool();
+
+	void renderPage(CharSequence source) {
+		final List<ImageInfo> imageInfos = scanForImageInfo(source);
+		Callable<List<ImageData>> task = new Callable<List<ImageData>>() {
+			public List<ImageData> call() {
+				List<ImageData> result = new ArrayList<ImageData>();
+				for (ImageInfo imageInfo : imageInfos)
+					result.add(imageInfo.downloadImage());
+				return result;
+			}
+		};
+
+		Future<List<ImageData>> future = executor.submit(task);
+		renderText(source);
+
+		try {
+			List<ImageData> imageData = future.get();
+			for (ImageData data : imageData)
+				renderImage(data);
+		} catch (InterruptedException e) {
+			// Re-assert the thread's interrupted status
+			Thread.currentThread().interrupt();
+			// We don't need the result, so cancel the task too
+			future.cancel(true);
+		} catch (ExecutionException e) {
+			throw LaunderThrowable.launderThrowable(e.getCause());
+		}
+	}
+
+	interface ImageData {
+	}
+
+	interface ImageInfo {
+		ImageData downloadImage();
+	}
+
+	abstract void renderText(CharSequence s);
+
+	abstract List<ImageInfo> scanForImageInfo(CharSequence s);
+
+	abstract void renderImage(ImageData i);
+}
