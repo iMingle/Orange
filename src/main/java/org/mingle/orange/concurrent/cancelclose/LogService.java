09ba5b72e329c91d5b65aa7f8aa4f15304e8076f
jinminglei <jinminglei@yeah.net>
2015-10-13 18:36:14 +0800

    取消与关闭线程

---
+++ src/main/java/org/mingle/orange/concurrent/cancelclose/LogService.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.cancelclose;
+
+import java.io.PrintWriter;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 支持关闭的生产者-消费者日志服务
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class LogService {
+	private static final int CAPACITY = 100;
+	private final BlockingQueue<String> queue;
+	private final LoggerThread loggerThread;
+	private final PrintWriter writer;
+	
+	private boolean isShutdown;
+	private int reservations;
+
+	public LogService(PrintWriter writer) {
+		this.queue = new LinkedBlockingQueue<>(CAPACITY);
+		this.writer = writer;
+		this.loggerThread = new LoggerThread();
+	}
+	
+	public void start() {
+		loggerThread.start();
+	}
+	
+	public void stop() {
+		synchronized (this) {
+			isShutdown = true;
+		}
+		loggerThread.interrupt();
+	}
+	
+	public void log(String msg) throws InterruptedException {
+		synchronized (this) {
+			if (isShutdown)
+				throw new IllegalStateException("is shutdown");
+			reservations++;
+		}
+		queue.put(msg);
+	}
+
+	private class LoggerThread extends Thread {
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					try {
+						synchronized (LogService.this) {
+							if (isShutdown && reservations == 0)
+								break;
+						}
+						String msg = queue.take();
+						synchronized (LogService.this) {
+							reservations--;
+						}
+						writer.println(msg);
+					} catch (InterruptedException e) {
+						/* retry */
+					}
+				}
+			} finally {
+				writer.close();
+			}
+		}
+	}
+}
+
+/**
+ * 关闭ExecutorService
+ */
+class LogService2 {
+	private final ExecutorService exec = Executors.newSingleThreadExecutor();
+	private final PrintWriter writer;
+	
+	public LogService2(PrintWriter writer) {
+		this.writer = writer;
+	}
+
+	public void stop() throws InterruptedException {
+		try {
+			exec.shutdown();
+			exec.awaitTermination(1, TimeUnit.SECONDS);
+		} finally {
+			writer.close();
+		}
+	}
+	
+	public void log(String msg) throws InterruptedException {
+		try {
+			exec.execute(new WriteTask(msg));
+		} catch (RejectedExecutionException ignored) {}
+	}
+	
+	private class WriteTask implements Runnable {
+		private String msg;
+
+		public WriteTask(String msg) {
+			this.msg = msg;
+		}
+		
+		@Override
+		public void run() {
+			writer.println(msg);
+		}
+	}
+}
---
+++ src/main/java/org/mingle/orange/concurrent/cancelclose/PrimeGenerator.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.cancelclose;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 素数生成器的取消
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class PrimeGenerator implements Runnable {
+	private final List<BigInteger> primes = new ArrayList<>();
+	private volatile boolean cancelled;
+
+	@Override
+	public void run() {
+		BigInteger p = BigInteger.ONE;
+		while (!cancelled) {
+			p = p.nextProbablePrime();
+			synchronized (this) {
+				primes.add(p);
+			}
+		}
+	}
+
+	public void cancel() {
+		cancelled = true;
+	}
+	
+	public synchronized List<BigInteger> get() {
+		return new ArrayList<>(primes);
+	}
+	
+	public static List<BigInteger> oneSecondOfPrimes() throws InterruptedException {
+		PrimeGenerator generator = new PrimeGenerator();
+		new Thread(generator).start();
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} finally {
+			generator.cancel();
+		}
+		
+		return generator.get();
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		PrimeGenerator.oneSecondOfPrimes();
+	}
+}
