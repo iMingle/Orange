7a7620735c1c453dfba22ff93b8bcbc97288ce76
jinminglei <jinminglei@yeah.net>
2015-10-14 16:29:45 +0800

    锁顺序导致的死锁

---
+++ src/main/java/org/mingle/orange/concurrent/livenessperformance/LeftRightDeadLock.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.livenessperformance;
+
+/**
+ * 简单的锁顺序死锁
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class LeftRightDeadLock {
+	private final Object left = new Object();
+	private final Object right = new Object();
+	
+	public void leftRight() {
+		synchronized (left) {
+			synchronized (right) {
+				doSomething();
+			}
+		}
+	}
+	
+	public void rightLeft() {
+		synchronized (right) {
+			synchronized (left) {
+				doSomethingElse();
+			}
+		}
+	}
+	
+	private void doSomething() {}
+	
+	private void doSomethingElse() {}
+}
