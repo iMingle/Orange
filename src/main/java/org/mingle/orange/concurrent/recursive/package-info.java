4fa59f3249d8c22d4da0ca79f739667eeb558829
jinminglei <jinminglei@yeah.net>
2015-10-14 13:55:08 +0800

    递归的并发解决方案

---
+++ src/main/java/org/mingle/orange/concurrent/recursive/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+/**
+ * 递归的并行化
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+package org.mingle.orange.concurrent.recursive;
---
+++ src/main/java/org/mingle/orange/concurrent/recursive/puzzle/SequentialPuzzleSolver.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.recursive.puzzle;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 串行解决方案
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class SequentialPuzzleSolver<P, M> {
+	private final Puzzle<P, M> puzzle;
+	private final Set<P> seen = new HashSet<>();
+	
+	public SequentialPuzzleSolver(Puzzle<P, M> puzzle) {
+		this.puzzle = puzzle;
+	}
+	
+	public List<M> solve() {
+		P pos = puzzle.initialPosition();
+		return search(new Node<P, M>(pos, null, null));
+	}
+
+	private List<M> search(Node<P, M> node) {
+		if (!seen.contains(node.pos)) {
+			seen.add(node.pos);
+			if (puzzle.isGoal(node.pos))
+				return node.asMoveList();
+			
+			for (M move : puzzle.legalMoves(node.pos)) {
+				P pos = puzzle.move(node.pos, move);
+				Node<P, M> child = new Node<P, M>(pos, move, node);
+				List<M> result = search(child);
+				if (result != null)
+					return result;
+			}
+		}
+		return null;
+	}
+	
+	static class Node<P, M> {
+		/**
+		 * prev -> move -> pos
+		 */
+		final P pos;
+		final M move;
+		final Node<P, M> prev;
+		
+		public Node(P pos, M move, Node<P, M> prev) {
+			this.pos = pos;
+			this.move = move;
+			this.prev = prev;
+		}
+		
+		public List<M> asMoveList() {
+			List<M> solution = new LinkedList<>();
+			for (Node<P, M> n = this; n.move != null; n = n.prev)
+				solution.add(n.move);
+			
+			return solution;
+		}
+	}
+}
