cfd166cf9bf950b8ac9aad79138bcf49cf691c22
jinminglei <jinminglei@yeah.net>
2015-10-19 18:21:50 +0800

    原子类CAS

---
+++ src/main/java/org/mingle/orange/concurrent/atomic/SimulatedCAS.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.concurrent.atomic;
+
+/**
+ * 
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class SimulatedCAS {
+	private int value;
+	
+	public synchronized int get() {
+		return value;
+	}
+	
+	public synchronized int compareAndSwap(int expectedValue, int newValue) {
+		int oldValue = value;
+		if (oldValue == expectedValue)
+			value = newValue;
+		return oldValue;
+	}
+	
+	public synchronized boolean compareAndSet(int expectedValue, int newValue) {
+		return expectedValue == compareAndSwap(expectedValue, newValue);
+	}
+}
---
+++ src/main/java/org/mingle/orange/concurrent/atomic/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+/**
+ * 原子变量与非阻塞同步机制
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+package org.mingle.orange.concurrent.atomic;
