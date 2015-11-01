003af5c72c6874cbd0a405148de1f6dc6f064ea8
jinminglei <jinminglei@yeah.net>
2015-11-01 12:39:25 +0800

    从无头单链表中删除节点

---
+++ src/main/java/org/mingle/orange/arithmetic/util/DeleteRandomNode.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.arithmetic.util;
+
+
+/**
+ * 从无头单链表中删除节点
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class DeleteRandomNode {
+	public static class Node<E> {
+		E data;
+		Node<E> next;
+	}
+	
+	public static void deleteRandomNode(Node<String> current) {
+		Node<String> next = current.next;
+		if (next != null) {
+			current.next = next.next;
+			current.data = next.data;
+			next = null;
+		}
+	}
+}
---
+++ src/test/java/org/mingle/orange/arithmetic/util/DeleteRandomNodeTests.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.arithmetic.util;
+
+import org.junit.Test;
+import org.mingle.orange.arithmetic.util.DeleteRandomNode.Node;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * 
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class DeleteRandomNodeTests {
+	@Test
+	public void deleteRandomNode() {
+		Node<String> head = new Node<>();
+		head.data = "HEAD";
+		Node<String> current = new Node<>();
+		current.data = "CURRENT";
+		head.next = current;
+		Node<String> next = new Node<>();
+		next.data = "NEXT";
+		current.next = next;
+		Node<String> tail = new Node<>();
+		tail.data = "TAIL";
+		next.next = tail;
+		tail.next = null;
+		
+		StringBuilder builder = new StringBuilder();
+		Node<String> tempHead = head;
+		
+		do {
+			builder.append(head.data).append("->");
+		} while ((head = head.next) != null);
+		
+		assertThat("HEAD->CURRENT->NEXT->TAIL->".equals(builder.toString())).isTrue();
+		builder.delete(0, builder.length());
+		
+		DeleteRandomNode.deleteRandomNode(current);
+		
+		do {
+			builder.append(tempHead.data).append("->");
+		} while ((tempHead = tempHead.next) != null);
+		
+		assertThat("HEAD->NEXT->TAIL->".equals(builder.toString())).isTrue();
+	}
+}
