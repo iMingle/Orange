452f43035373da4e96c5e131230b86f65c522181
jinminglei <jinminglei@yeah.net>
2015-11-03 08:35:00 +0800

    重建二叉树完成

---
+++ src/main/java/org/mingle/orange/arithmetic/util/RebuildBTree.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.arithmetic.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 根据前序和中序遍历结果重建二叉树
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class RebuildBTree<E> {
+	public static class Node<E> {
+		Node<E> left;
+		Node<E> right;
+		E data;
+	}
+	
+	/**
+	 * @param preorder 前序遍历结果
+	 * @param inorder 中序遍历结果
+	 * @param treeLength 树的节点个数
+	 * @param root 根节点
+	 */
+	public void rebuild(String preorder, String inorder, int treeLength, Node<Character> root) {
+		if (StringUtils.isBlank(preorder) || StringUtils.isBlank(inorder))
+			return;
+		
+		// 已经是最后一个节点
+		if (treeLength == 1)
+			return;
+		
+		// 寻找子树长度
+		String orginInorder = inorder;
+		String leftEnd = inorder;
+		int tempLength = 0;
+		int i = 0;
+		
+		// 找到左子树的结尾
+		while (preorder.charAt(0) != leftEnd.charAt(i)) {
+			tempLength++;
+			if (tempLength > treeLength)
+				break;
+			i++;
+		}
+		
+		// 寻找左子树长度
+		int leftLength = 0;
+		leftLength = i;
+		
+		// 寻找右子树长度
+		int rightLength = 0;
+		rightLength = treeLength - leftLength - 1;
+		
+		// 重建左子树
+		if (leftLength > 0) {
+			Node<Character> left = new Node<>();
+			left.data = preorder.charAt(1);
+			root.left = left;
+			rebuild(preorder.substring(1), orginInorder, leftLength, left);
+		}
+		
+		// 重建右子树
+		if (rightLength > 0) {
+			Node<Character> right = new Node<>();
+			right.data = preorder.charAt(leftLength + 1);
+			root.right = right;
+			rebuild(preorder.substring(leftLength + 1), orginInorder.substring(leftLength + 1), rightLength, right);
+		}
+	}
+}
---
+++ src/test/java/org/mingle/orange/arithmetic/util/RebuildBTreeTests.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.arithmetic.util;
+
+import org.junit.Test;
+import org.mingle.orange.arithmetic.util.RebuildBTree.Node;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * 
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class RebuildBTreeTests {
+	@Test
+	public void rebuild() {
+		RebuildBTree<Character> bTree = new RebuildBTree<>();
+		String preorder = "abdcef";
+		String inorder = "dbaecf";
+		Node<Character> root = new Node<>();
+		root.data = preorder.charAt(0);
+		bTree.rebuild(preorder, inorder, 6, root);
+		
+		assertThat(root.left.data).isEqualTo('b');
+		assertThat(root.left.left.data).isEqualTo('d');
+		assertThat(root.right.data).isEqualTo('c');
+		assertThat(root.right.left.data).isEqualTo('e');
+		assertThat(root.right.right.data).isEqualTo('f');
+		
+	}
+}
