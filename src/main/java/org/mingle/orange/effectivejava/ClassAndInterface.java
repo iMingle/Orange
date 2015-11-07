471c113c808be391f1c121e1bc542cc234a83fc6
jinminglei <jinminglei@yeah.net>
2015-11-07 22:02:57 +0800

    1. 使类和成员的可访问性最小化 2. 不可变类设计 3. Object的通用方法设计

---
+++ src/main/java/org/mingle/orange/effectivejava/ClassAndInterface.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.effectivejava;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 类和接口
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class ClassAndInterface {
+	// 基本数据类型或不可变类
+	private static final Integer[] PRIVATE_VALUES = {Integer.valueOf(0), Integer.valueOf(1)
+	};
+	public static final List<Integer> VALUES = Collections.unmodifiableList(Arrays.asList(PRIVATE_VALUES));
+	
+	public static final Integer[] values() {
+		return PRIVATE_VALUES.clone();
+	}
+	
+}
