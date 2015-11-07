0d6bc21eacea3c9c9e43a23751dad6660f3c8f55
jinminglei <jinminglei@yeah.net>
2015-11-07 11:21:30 +0800

    创建和销毁对象

---
+++ src/main/java/org/mingle/orange/effectivejava/CreateAndDestroyObject.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2015, Mingle. All rights reserved.
+ */
+package org.mingle.orange.effectivejava;
+
+/**
+ * 创建和销毁对象
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class CreateAndDestroyObject {
+	
+	/**
+	 * 考虑用静态工厂代替构造器
+	 * 
+	 * @param b
+	 * @return
+	 */
+	public static Boolean valueOf(boolean b) {
+		return b ? Boolean.TRUE : Boolean.FALSE;
+	}
+	
+}
