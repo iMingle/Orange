f546c7c793190fcecff29a292fc60b4cb6901fb9
jinminglei <jinminglei@yeah.net>
2015-12-10 19:45:10 +0800

    ActiveMQ测试代码添加

---
+++ src/main/java/org/mingle/orange/message/ActiveMQMessageProducer.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2016, Mingle. All rights reserved.
+ */
+package org.mingle.orange.message;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+
+/**
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class ActiveMQMessageProducer {
+
+	public static void main(String[] args) {
+		ConnectionFactory cf = new ActiveMQConnectionFactory();
+		Connection conn = null;
+		Session session = null;
+		try {
+			conn = cf.createConnection();
+			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = new ActiveMQQueue("messageQueue");
+			MessageProducer producer = session.createProducer(destination);
+			TextMessage message = session.createTextMessage();
+			message.setText("hello");
+			producer.send(message);
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (session != null)
+					session.close();
+				if (conn != null)
+					conn.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/main/java/org/mingle/orange/message/ActiveMQMessageReceiver.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2016, Mingle. All rights reserved.
+ */
+package org.mingle.orange.message;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+
+/**
+ * 
+ * @since 1.8
+ * @author Mingle
+ */
+public class ActiveMQMessageReceiver {
+
+	public static void main(String[] args) {
+		ConnectionFactory cf = new ActiveMQConnectionFactory();
+		Connection conn = null;
+		Session session = null;
+		try {
+			conn = cf.createConnection();
+			conn.start();
+			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = new ActiveMQQueue("messageQueue");
+			MessageConsumer consumer = session.createConsumer(destination);
+			Message message = consumer.receive();
+			TextMessage textMessage = (TextMessage) message;
+			System.out.println("Get a message: " + textMessage.getText());
+			conn.start();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (session != null)
+					session.close();
+				if (conn != null)
+					conn.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
