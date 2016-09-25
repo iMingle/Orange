9a633ac4cf5c6bc1b48f60813ad9438f1f358dcc
jinminglei <jinminglei@yeah.net>
2016-09-25 23:13:22 +0800

    add selenium

---
+++ src/main/java/org/mingle/orange/autotest/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2016, Mingle. All rights reserved.
+ */
+/**
+ * 自动化测试
+ *
+ * @since 1.0
+ * @author Mingle
+ */
+package org.mingle.orange.autotest;
---
+++ src/main/java/org/mingle/orange/autotest/selenium/WebDriverTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2016, Mingle. All rights reserved.
+ */
+package org.mingle.orange.autotest.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ *
+ * @since 1.0
+ * @author Mingle
+ */
+public class WebDriverTest {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //Puts a Implicit wait, Will wait for 10 seconds before throwing exception
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://www.google.com");
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.id("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            @Override public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+
+        //Close the browser
+        driver.quit();
+    }
+
+}
