71abfe53e65de5cead31d306a2df7815d80fd895
jinminglei <jinminglei@yeah.net>
2016-12-11 15:59:41 +0800

    modify copyright

--- src/main/java/org/mingle/orange/java/speciality/NullObject.java
+++ src/main/java/org/mingle/orange/java/speciality/NullObject.java
@@ -1,5 +1,4 @@
 /*
- *
  * Copyright 2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * imitations under the License.
- *
+ * limitations under the License.
  */
 
 package org.mingle.orange.java.speciality;
