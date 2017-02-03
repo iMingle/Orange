780e70d27c7f60373aa3cae103fe3e3fa3c00413
jinminglei <jinminglei@meicai.cn>
2017-02-03 19:03:30 +0800

    add channel test

--- src/main/java/org/mingle/orange/java/nio/BufferTest.java
+++ src/main/java/org/mingle/orange/java/nio/BufferTest.java
@@ -16,30 +16,36 @@
 
 package org.mingle.orange.java.nio;
 
+import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 
 /**
- *
  * @author mingle
  */
 public class BufferTest {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+        byteBuffer.put((byte) 'H').put((byte) 'e').put((byte) 'l').put((byte) 'l').put((byte) 'o');
+        byteBuffer.put(0, (byte) 'M').put((byte) 'w');
+        byteBuffer.flip();
+        System.out.println(byteBuffer.mark());
+        System.out.println(byteBuffer.position());
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.capacity());
+
         CharBuffer buffer = CharBuffer.allocate(5);
         buffer.put('1');
         buffer.put('2');
         buffer.put('3');
-        
+
         buffer.flip();
-        
+
         buffer.put('4');
         buffer.put('5');
-        
+
         buffer.clear();
-        
+
         System.out.println(buffer.get());
         System.out.println(buffer.get());
         System.out.println(buffer.get());
