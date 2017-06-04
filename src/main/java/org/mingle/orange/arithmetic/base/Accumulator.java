2b3fb68fb9958fd284c0fe82fd786fec137109ac
jinminglei <jinminglei@yeah.net>
2017-06-04 10:19:09 +0800

    add union find

--- src/main/java/org/mingle/orange/arithmetic/base/Accumulator.java
+++ src/main/java/org/mingle/orange/arithmetic/base/Accumulator.java
@@ -3,12 +3,12 @@ package org.mingle.orange.arithmetic.base;
 public class Accumulator {
     private double total;
     private int N;
-    
+
     public void addDataValue(double val) {
         N++;
         total += val;
     }
-    
+
     public double mean() {
         return total / N;
     }
@@ -16,12 +16,8 @@ public class Accumulator {
     public String toString() {
         return "Mean (" + N + " values): " + String.format("%7.5f", mean());
     }
-    
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.println("Hello");
     }
 
