12fbc91c51720b0fa0f34b44d1b9f0a3f35139fd
jinminglei <jinminglei@yeah.net>
2019-07-07 19:57:35 +0800

    add flink and beam

--- src/main/java/org/mingle/orange/java7/BinaryIntegerLiteral.java
+++ src/main/java/org/mingle/orange/java7/BinaryIntegerLiteral.java
@@ -17,8 +17,6 @@
 package org.mingle.orange.java7;
 
 /**
- * 
- * 
  * @author mingle
  */
 public class BinaryIntegerLiteral {
@@ -28,7 +26,7 @@ public class BinaryIntegerLiteral {
         byte b3 = 041;
         byte b4 = 33;
         System.out.println((b1 == b2) && (b3 == b4));
-        
+
         long maxInt = 21_474_836_47;
         System.out.println(Integer.MAX_VALUE == maxInt);
     }
